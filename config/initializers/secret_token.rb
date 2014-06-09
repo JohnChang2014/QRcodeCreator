48748eefb57669ff693faec90821ee64d31ef157
John Chang <johnchang2014@gmail.com>
2014-06-09 18:33:43 -0400

    added description of welcome page feature to the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QRcodeCreator::Application.config.secret_key_base = '21437470ed7c1b8e3c85cc4bc206a78894b5678da41a0c64883720fd0724fee783081d2c4f088067bcf6096435c087a5dc21d5135c5a5792efdee028e2b9b032'
---
+++ features/welcome_page.feature
@@ -0,0 +1,9 @@
+Feature: welcome page
+  So that I can start to generate a qrcode
+  As a qrcode-user
+  I want to visit welcome page of QR code generator
+  
+  Scenario: shows welcome page to users
+    When users visit the welcome page of QR code generator
+    Then the page should say "Welcome to QR code generator"
+    And the page should have a text field with the title of "Please enter any message for your QR code below:"
