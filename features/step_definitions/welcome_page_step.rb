2664adcddc1d13372dc8c8c15e78b622ba3177ee
John Chang <johnchang2014@gmail.com>
2014-06-09 18:41:20 -0400

    added root_path and created welcome controller for the welcome step

---
+++ features/step_definitions/welcome_page_step.rb
@@ -0,0 +1,11 @@
+When(/^users visit the welcome page of QR code generator$/) do
+	visit root_path
+end
+
+Then(/^the page should say "(.*?)"$/) do |arg1|
+	pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the page should have a text field with the title of "(.*?)"$/) do |arg1|
+	pending # express the regexp above with the code you wish you had
+end
