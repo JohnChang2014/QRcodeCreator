18d327f6d8561c747f6afecd03275a9fb02aa23a
John Chang <johnchang2014@gmail.com>
2014-06-09 18:53:39 -0400

    added welcome view spec and made it pass in spec and step file

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe "welcome/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "shows a title of 'Welcome to QR code generator'" do
+		render
+		expect(rendered).to have_content('Welcome to QR code generator')
+	end
 end
