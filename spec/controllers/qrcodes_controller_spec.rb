ada76f64102aecfad505733c16341a19f1bb847f
John Chang <johnchang2014@gmail.com>
2014-06-10 20:42:56 -0400

    revised qrcode_step.rb

--- spec/controllers/qrcodes_controller_spec.rb
+++ spec/controllers/qrcodes_controller_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 describe QrcodesController do
-	describe "POST 'create'" do 
+	describe "POST 'create'" do
 		it "returns http success" do
 			post 'create'
 			expect(response).to be_success
 		end
-		
+
 		it "renders QR code image in the response" do
 			post 'create'
-			@qrcode = QRcode.new 'hello'
-			expect(response).to 
+			cqrcode = RQRCode::CQRCode.new
+			options = { :info => "hello" }
+			data    = cqrcode.generateQRcode(options, :png)
+			expect(response.body).to eq data
 		end
 	end
 end
